3bfd940fc5013b5a7ca4d59d2ae696970c98ea7c
Pelomedusa <alfredossic@hotmail.fr>
2017-01-16 23:01:12 +0100

    a commit message

---
+++ database/migrations/2017_01_16_213022_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->integer('sender');
+            $table->integer('receiver');
+            $table->text('body');
+            $table->foreign('sender')->references('id')->on('users');
+            $table->foreign('receiver')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
