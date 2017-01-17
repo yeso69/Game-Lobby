8b46758158d814b0a4a12595e252b740d221b8f2
Pelomedusa <alfredossic@hotmail.fr>
2017-01-17 07:35:42 +0100

    fix css

--- resources/views/pages/browse_players.blade.php
+++ resources/views/pages/browse_players.blade.php
@@ -16,7 +16,7 @@
     <script type="text/javascript" src="{{ URL('/js/player_card.js') }}"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 @endsection
-@section('title', '| Browse players')
+@section('title', '|Browse players')
 
 @section('content')
 
